801eb0676dc0d79dd1df1a06093d92a93e953260
Juhi <257458510+juhidwivedi19@users.noreply.github.com>
2026-07-28 19:19:17 +0530

    create organization controller

--- src/controllers/organization.controller.js
+++ src/controllers/organization.controller.js
@@ -1,2 +1,58 @@
 const prisma = require("../config/db.config.js");
 
+
+async function createOrganizationController(req,res) {
+     try{
+    const {name} = req.body;
+
+    //GET LOGGED IN USER
+     const userId= req.user.userId;
+
+     //check organization name
+     if(!name){
+        return res.status(400).json({
+            message:"Organization name is required",
+            status: "failed"
+        });
+     }
+
+     //Create Organization
+
+     const organization = await prisma.organization.create({
+        data:{
+            name:name
+        }
+     });
+
+     //create organization member
+
+     await prisma.organizationMember.create({
+        data:{
+            userId: userId,
+            organizationId: organization.id,
+            role:"OWNER"
+        }
+     });
+
+     //Send response
+
+     return res.status(201).json({
+        message: "Organization created successfully",
+        status:"success",
+        organization:{
+            id: organization.id,
+            name: organization.name
+        }
+     });
+
+     }catch(error){
+        return res.status(500).json({
+            message: error.message,
+            status: "failed"
+        });
+     }
+}
+
+module.exports={
+    createOrganizationController
+}
