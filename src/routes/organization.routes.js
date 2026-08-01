8bc3b7b9c807225257848b1923e5573f1c828b5e
Juhi Dhar Dwivedi <257458510+juhidwivedi19@users.noreply.github.com>
2026-08-01 16:02:11 +0530

    Update organization.routes.js

--- src/routes/organization.routes.js
+++ src/routes/organization.routes.js
@@ -2,25 +2,23 @@ const express = require("express");
 const organizationController = require("../controllers/organization.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 const organizationAuthorizationMiddleware = require("../middlewares/organizationAuthorization.middleware");
-const roleAuthorizationMiddleware = require("../middlewares/roleAuthorization.middleware");  //add these middleware first then router.post
+const roleAuthorizationMiddleware = require("../middlewares/roleAuthorization.middleware"); 
 
 const router = express.Router();
 
-//Create organization
 router.post(
     "/",
     authMiddleware.authMiddleware,
     organizationController.createOrganizationController
 );
 
-//Get my organization
 router.get(
     "/",
     authMiddleware.authMiddleware,
     organizationController.getMyOrganizationController
 );
 
-//Get organization details
+
 
 router.get(
     "/:organizationId",
