bddff718e803914b986f5f408d4872b1483b655f
Juhi Dhar Dwivedi <257458510+juhidwivedi19@users.noreply.github.com>
2026-08-01 22:44:25 +0530

    Update organization.controller.js

--- src/controllers/organization.controller.js
+++ src/controllers/organization.controller.js
@@ -176,13 +176,12 @@ async function getOrganizationDetailsController(req, res) {
 
 async function inviteMemberController(req,res){
     try{
-      // 1. Get organizationId from req.params
+     
       const organizationId = parseInt(req.params.organizationId);
 
-        // 2. Get email and role from req.body
       const{email,role} = req.body;
 
-        // 3. Validate input
+     
         if(!email || !role){
              return res.status(400).json({
                 message: "Email and role are required",
@@ -190,22 +189,20 @@ async function inviteMemberController(req,res){
             });
         }
 
-        // 4. Check if user already belongs to organization
-
-          // First find the user using the email:
+     
          const user = await prisma.user.findUnique({
         where: {
         email: email
          }
        });
-         // If the user doesn't exist:
+     
         if (!user) {
             return res.status(404).json({
                 message: "User not found",
                 status: "failed"
             });
-        }
-            // Now check if the user is already a member of the organization:
+        }            
+
          const existingMember = await prisma.organizationMember.findUnique({
     where: {
         userId_organizationId: {
@@ -214,7 +211,7 @@ async function inviteMemberController(req,res){
         }
     }
 });
-          //if aslready a member, return an error
+        
           if (existingMember) {
               return res.status(400).json({
              message: "User is already a member of this organization",
@@ -222,15 +219,15 @@ async function inviteMemberController(req,res){
     });
      }
 
-        // 5. Generate invitation token  using crypto and token in upper start
+    
           
         const token = crypto.randomBytes(32).toString("hex");
 
-        // 6. Set expiry
+       
            const expiryDate = new Date();
-           expiryDate.setDate(expiryDate.getDate() + 1); // Set expiry to 1 day from now
+           expiryDate.setDate(expiryDate.getDate() + 1); 
 
-        // 7. Create invitation
+      
        const invitation = await prisma.invitation.create({
          data: {
         email: email,
@@ -241,7 +238,7 @@ async function inviteMemberController(req,res){
     }
       });
 
-        // 8. Send invitation email
+     
          
         const invitationLink =`http://localhost:4000/api/organizations/accept-invitation?token=${token}`;
                  await sendInvitationEmail(
@@ -251,7 +248,7 @@ async function inviteMemberController(req,res){
          );
 
 
-        // 9. Return response
+        
          return res.status(201).json({
            message: "Invitation sent successfully",
            status: "success"
