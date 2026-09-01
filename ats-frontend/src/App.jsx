4bdef98114ec4b454b3e2b193355f1e3ae3c3b6f
Juhi <257458510+juhidwivedi19@users.noreply.github.com>
2026-09-01 11:50:38 +0530

    started frontend part

---
+++ ats-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+function App(){
+  return(
+     <>
+            <Navbar />
+
+            <main>
+                <h1>Modern ATS</h1>
+                <p>Applicant Tracking System</p>
+            </main>
+        </>
+  );
+}
+
+export default App;
